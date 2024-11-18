e050bfd4c7d05d7f4b94b6ae372b19769fb4222a
thulanigamtee <thulanigamtee@gmail.com>
2024-11-18 21:31:57 +0200

    deleted item repository

--- src/main/java/com/invoice/invoice_api/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.invoice.invoice_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.invoice.invoice_api.Model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-}
