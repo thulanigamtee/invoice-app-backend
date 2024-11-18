d60ced48122fcdb4777886e0495b33507ee6ba99
thulanigamtee <thulanigamtee@gmail.com>
2024-11-18 21:31:42 +0200

    deleted address repository

--- src/main/java/com/invoice/invoice_api/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.invoice.invoice_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.invoice.invoice_api.Model.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
