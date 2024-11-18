c2e0cf5aea464262fd10eab0d19c782689e015bb
thulanigamtee <thulanigamtee@gmail.com>
2024-11-18 21:48:23 +0200

    fixed null and override issue

--- src/main/java/com/invoice/invoice_api/repository/InvoiceRepository.java
+++ src/main/java/com/invoice/invoice_api/repository/InvoiceRepository.java
@@ -3,9 +3,10 @@ package com.invoice.invoice_api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.invoice.invoice_api.Model.Invoice;
+import com.invoice.invoice_api.Models.Invoice;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, String> {
-    boolean existsById(@SuppressWarnings("null") String id);
+    @Override
+    boolean existsById(String id);
 }
