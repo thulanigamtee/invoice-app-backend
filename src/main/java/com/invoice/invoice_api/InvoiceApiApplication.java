79de987372a7484c0a738a24d745ced519b42214
thulanigamtee <thulanigamtee@gmail.com>
2025-01-08 04:38:07 +0200

    prepare for production

--- src/main/java/com/invoice/invoice_api/InvoiceApiApplication.java
+++ src/main/java/com/invoice/invoice_api/InvoiceApiApplication.java
@@ -2,10 +2,9 @@ package com.invoice.invoice_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:${user.dir}/.env")
+// @PropertySource("file:${user.dir}/.env")
 public class InvoiceApiApplication {
 
 	public static void main(String[] args) {
