92024c7fab47e51fb4b6695ac38ae3099a62d5ea
thulanigamtee <thulanigamtee@gmail.com>
2025-02-01 04:30:52 +0200

    remove .env property source

--- src/main/java/com/invoice/invoice_api/InvoiceApiApplication.java
+++ src/main/java/com/invoice/invoice_api/InvoiceApiApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// @PropertySource("file:${user.dir}/.env")
 public class InvoiceApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceApiApplication.class, args);
 	}
-
 }
